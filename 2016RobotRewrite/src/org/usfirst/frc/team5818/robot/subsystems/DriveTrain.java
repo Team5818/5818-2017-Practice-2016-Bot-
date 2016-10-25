883a558ea0a26fc5cbd4cdad7ba7a31da73f8f80
km00700 <km00700@gmail.com>
2016-10-24 17:53:17 -0700

    cleaned up talons

--- 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/subsystems/DriveTrain.java
+++ 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/subsystems/DriveTrain.java
@@ -23,6 +23,10 @@ public class DriveTrain extends Subsystem {
 		TALON_MOTOR_FL = new CANTalon(BotConstants.TALON_FL);
 		TALON_MOTOR_BL = new CANTalon(BotConstants.TALON_BL);
 		TALON_MOTOR_ML = new CANTalon(BotConstants.TALON_MR);
+		
+		TALON_MOTOR_FR.setInverted(true);
+		TALON_MOTOR_BR.setInverted(true);
+		TALON_MOTOR_MR.setInverted(true);
 	}
 	
 	public static void drive() {
@@ -64,9 +68,9 @@ public class DriveTrain extends Subsystem {
 	}
 	
 	public static void setRightVelocity(double numIn) {
-    	TALON_MOTOR_FR.set(-1*numIn*BotConstants.VEL_MULTIPLIER);
-    	TALON_MOTOR_BR.set(-1*numIn*BotConstants.VEL_MULTIPLIER);
-    	TALON_MOTOR_MR.set(-1*numIn*BotConstants.VEL_MULTIPLIER);
+    	TALON_MOTOR_FR.set(numIn*BotConstants.VEL_MULTIPLIER);
+    	TALON_MOTOR_BR.set(numIn*BotConstants.VEL_MULTIPLIER);
+    	TALON_MOTOR_MR.set(numIn*BotConstants.VEL_MULTIPLIER);
     }
     
     public static void setLeftVelocity(double numIn) {
