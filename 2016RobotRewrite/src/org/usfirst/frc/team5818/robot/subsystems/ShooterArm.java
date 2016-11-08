b69da621b063ec1cbe87756ef44ed5d7ac2f591e
JacksonMcLaughlin <jacksonmclaughlin1@gmail.com>
2016-11-07 18:44:20 -0800

    PID control for ShooterArm

--- 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/subsystems/ShooterArm.java
+++ 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/subsystems/ShooterArm.java
@@ -3,24 +3,41 @@ package org.usfirst.frc.team5818.robot.subsystems;
 import org.usfirst.frc.team5818.constants.BotConstants;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 
-public class ShooterArm extends Subsystem {
+public class ShooterArm extends Subsystem implements PIDOutput{
     
 	private static CANTalon ARM_MOTOR_0;
 	private static CANTalon ARM_MOTOR_1;
 
-	public static void initializeShooterArm() {
+	public ShooterArm() {
 		ARM_MOTOR_0 = new CANTalon(BotConstants.TALON_ARM_0);
 		ARM_MOTOR_1 = new CANTalon(BotConstants.TALON_ARM_1);
 		
 		ARM_MOTOR_1.setInverted(true);
 	}
 
-	public static void setArmPower(double numIn) {
+	public void setArmPower(double numIn) {
 		ARM_MOTOR_0.set(numIn * BotConstants.ARM_POWER_MULTIPLIER);
 		ARM_MOTOR_1.set(numIn * BotConstants.ARM_POWER_MULTIPLIER);
 	}
+	private static final double PID_KP = 1.0; ///////////////
+	private static final double PID_KI = 1.0; // CHANGE ME //
+	private static final double PID_KD = 1.0; ///////////////
+	
+	public PIDController armController = new PIDController(PID_KP, PID_KI, PID_KD, ARM_MOTOR_0, this);
+	
     public void initDefaultCommand() {
     	
     }
+
+	@Override
+	public void pidWrite(double output) {
+		setArmPower(output);
+	}
+	public void setArmPosition(double target) {
+		armController.setSetpoint(target);
+		armController.enable();
+	}
 }
