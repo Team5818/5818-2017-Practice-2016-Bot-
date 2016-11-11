ca8776bbf1ada68ba356d9c3e1e8e04fe4110be6
km00700 <km00700@gmail.com>
2016-11-11 10:47:28 -0800

    Cleaned code. Added initialization phase.

--- 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/Robot.java
+++ 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/Robot.java
@@ -107,9 +107,6 @@ public class Robot extends IterativeRobot {
         // continue until interrupted by another command, remove
         // this line or comment it out.
         if (autonomousCommand != null) autonomousCommand.cancel();
-        //cTalFR.reverseOutput(true);
-        //cTalMR.reverseOutput(true);
-        //cTalBR.reverseOutput(true);
         Driver.driverInit();
     }
 
