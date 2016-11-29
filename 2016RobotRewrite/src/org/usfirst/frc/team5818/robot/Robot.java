4d560421643eac4f5c63effd026493d7ab208117
km00700 <km00700@gmail.com>
2016-11-28 17:45:13 -0800

    Removed Old Sloppy Code

--- 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/Robot.java
+++ 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/Robot.java
@@ -31,7 +31,6 @@ public class Robot extends IterativeRobot {
 	//private static boolean buttonUp;
 	
 	public static OI oi;
-
 	
 	
 	//public static Joystick controlJoyFB = new Joystick(0);
@@ -51,11 +50,12 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
+		
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
-        DriveTrain.initializeDriveTrain();
+        drivetrain.initializeDriveTrain();
     }
 	
 	/**
