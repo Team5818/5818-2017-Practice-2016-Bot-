970e88de0f1975b3d78dc694196a71dc9dc5d26d
JacksonMcLaughlin <jacksonmclaughlin1@gmail.com>
2016-12-09 11:46:58 -0800

    indent fix

--- 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/commands/DriveControlCommand.java
+++ 2016RobotRewrite/src/org/usfirst/frc/team5818/robot/commands/DriveControlCommand.java
@@ -10,31 +10,31 @@ public class DriveControlCommand extends Command{
 	private DriveTrain dTrain;
 	
 	public DriveControlCommand(DriveTrain train) {
-        requires(Robot.driveTrain);
-        dTrain = train;
-    }
+		requires(Robot.driveTrain);
+		dTrain = train;
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	dTrain.initializeDriveTrain();
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		dTrain.initializeDriveTrain();
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	dTrain.drive();
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		dTrain.drive();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-    
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
